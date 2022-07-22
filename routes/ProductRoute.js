33f37f0a9d166e5dcff060f648ac4d7d208dca93
gokul1630 <gokulgp3167@gmail.com>
2022-07-22 20:30:20 +0530

    Removed Token Auth for Products

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -9,7 +9,7 @@ const {
 } = require('../controllers/ProductController')
 
 router.post('/update', auth, updateProduct)
-router.get('/', auth, getProducts)
+router.get('/', getProducts)
 router.get('/:categoryId', auth, getProductByCategory)
 router.put('/add', auth, addProducts)
 router.delete('/remove', auth, deleteProduct)
