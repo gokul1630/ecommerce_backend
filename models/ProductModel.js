8ca8c718d1b8f8ebf6ac9fd815df11eaef70ba96
gokul1630 <gokulgp3167@gmail.com>
2022-07-22 15:57:10 +0530

    Added More details to Product Schema

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose')
 const productSchema = mongoose.Schema({
   brand: { type: String },
   description: { type: String, required: true },
-  imageLink: { type: String },
+  image: { type: String, required: true },
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  stocks: { type: Number, required: true, default: 1 },
+  stocks: { type: Number, required: true, default: 0 },
   categoryId: { type: mongoose.Types.ObjectId, ref: 'Category' },
+  ratings: { type: String },
+  images: [{ type: String }],
 })
 
 module.exports = mongoose.model('Products', productSchema)
