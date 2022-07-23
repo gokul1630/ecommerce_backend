d2d05a09498deb3251b52b909bc786dff7cbe351
gokul1630 <gokulgp3167@gmail.com>
2022-07-23 14:53:55 +0530

    Updated constants

--- constants/constants.js
+++ constants/constants.js
@@ -6,6 +6,7 @@ module.exports = Object.freeze({
   CART_IS_EMPTY: 'Cart is Empty',
   ITEM_ALREDY_IN_CART: 'Item Already Added To Cart',
   ITEM_DELETED: 'Item Deleted',
+  ITEM_NOT_FOUND: 'Item Not Found',
   INVALID_PASSWORD_WARNING:
     'Password must be Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character',
   INVALID_EMAIL_WARNING: 'Provide Valid Email',
@@ -16,4 +17,8 @@ module.exports = Object.freeze({
   INVALID_TOKEN: 'Invalid Token',
   DEFAULT_USER_AVATAR:
     'https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg',
+  PRODUCT_DELETED: 'Product Deleted',
+  PRODUCT_NOT_FOUND: 'Product Not Found',
+  CATEGORY_NOT_FOUND:'Category Not Found',
+  CATEGORY_DELETED:'Category Deleted',
 })
